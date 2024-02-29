d1f757688f5affdf90cfcf0da631804dd32c56c7
TagwazaBesa <besaemmanuel99@gmail.com>
2024-02-29 19:20:39 +0200

    LATESTT

---
+++ WilaWane/contacts.php
@@ -0,0 +1,39 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+// Include database connection
+include("includes/db.php");
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                       // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'besaemmanuel@gmail.com';             // SMTP username
+    $mail->Password   = 'uzlj bakf ufhd nanx';                         // SMTP password
+    $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom($_POST['email'], $_POST['name']);
+    $mail->addAddress('1804685@northrise.net', 'Recipient Name');   // Add a recipient
+
+    // Content
+    $mail->isHTML(true);                                        // Set email format to HTML
+    $mail->Subject = $_POST['subject'];
+    $mail->Body    = strip_tags($_POST['message']);
+    $mail->AltBody = strip_tags($_POST['message']);
+
+    $mail->send();
+    echo 'Message has been sent. We will get back to you!';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+?>
